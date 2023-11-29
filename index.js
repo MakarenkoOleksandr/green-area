7c485c76f0cdcd7a5507a9984140a7d65befcc9a
MakarenkoOleksandr <majop11111@gmail.com>
2023-11-29 13:34:28 +0100

    started project

--- index.js
+++ index.js
@@ -5,5 +5,20 @@ const bot = new TelegramBot("6987337400:AAHp2b4E7CngidB0nBFOqix6xW2rmQMp59E", {
 
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
-  bot.sendMessage(chatId, "Привет! Я твой бот.");
+
+  const keyboard = {
+    reply_markup: {
+      keyboard: [
+        ["🛒 Каталог товаров", "ℹ️ О нас"],
+        ["📊 Статистика", "📞 Контакты"],
+      ],
+      resize_keyboard: true,
+    },
+  };
+
+  bot.sendMessage(
+    chatId,
+    `Привет! Добро пожаловать в наш магазин. Как я могу помочь?`,
+    keyboard
+  );
 });
